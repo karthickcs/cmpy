6303b865ef7a81d99b29c3cc2bdea3f723273bfd
karthickcs <cs.karthick@gmail.com>
2024-08-08 10:12:21 +0530

    add

--- typesc/typescript-angular-client-generated (1)/typescript-angular-client/model/models.ts
+++
@@ -1,6 +0,0 @@
-export * from './diffTableDTO';
-export * from './dpListenDTO';
-export * from './jwtRequest';
-export * from './responseEntity';
-export * from './taskMainDTO';
-export * from './userDTO';
